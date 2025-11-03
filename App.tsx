84578b310ecbc651968b6da2f7efe83790422897
palanidurai9 <palaniramesh079@gmail.com>
2025-11-03 11:29:47 +0530

    add name domain

--- App.tsx
+++ App.tsx
@@ -66,7 +66,7 @@ const App: React.FC = () => {
           </ScrollReveal>
           <ScrollReveal delay={0.4}>
             <a 
-              href="https://luxefiz.netlify.app/" 
+              href="https://luxefiz.vercel.app/" 
               target="_blank" 
               rel="noopener noreferrer"
               className="group inline-block mt-10 px-8 py-3 rounded-full border border-[#9f009f] text-[#9f009f] font-medium text-base transition-all duration-300 ease-in-out hover:bg-[#9f009f] hover:text-white"
